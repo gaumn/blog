28f253990962b6bbc4179bc6aba61aad9c8816b8
gaumn <gaumn@outlook.com>
2022-02-09 14:00:03 +0800

    解决前后端交互的跨域问题

--- blog_java/src/main/java/com/invain/BlogApplication.java
+++ blog_java/src/main/java/com/invain/BlogApplication.java
@@ -1,8 +1,10 @@
 package com.invain;
 
-import org.mybatis.spring.annotation.MapperScan;
+import com.invain.config.Filter.CrosFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
@@ -10,6 +12,17 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+    /**
+     * 配置跨域访问的过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean registerFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new CrosFilter());
+        return bean;
     }
 
 }
