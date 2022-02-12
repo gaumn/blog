f0d2dd33f2cc0dc713c3b8ee6f8b52f8115eb355
gaumn <gaumn@outlook.com>
2022-02-12 15:55:17 +0800

    新增滚动效果

--- blog_vue/src/views/Blogs.vue
+++ blog_vue/src/views/Blogs.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: gaumn 
  * @Date: 2022-02-06 09:55:08
- * @LastEditTime: 2022-02-10 15:00:34
+ * @LastEditTime: 2022-02-12 15:54:08
  * @LastEditors: gaumn
 -->
 <template>
@@ -10,20 +10,19 @@
   
   <div class="blogs">
     <NavigationBar></NavigationBar>
-    首页
-  <div class="container">
-    <div class="panel panel-default"  v-for="it in blogs" :key="it.id" >
-      
-    <div class="panel-body">
-      
-        {{it.id}}
-      
-   </div>
-</div>
+            首页
+    <div class="container" >
+      <div v-for="array in shows" :key="array">
+        <div class="panel panel-default" v-for="it in array" :key="it">
+           <div class="panel-body">
+             {{it}}
+           </div>
+        </div>
+      </div>
+    <div v-show="isLoading&&!noPage">isLoading</div>
+    <div v-show="noPage">noPage</div>
   </div>
-  
- </div>
-
+  </div>
 </template>
 
 <script>
@@ -35,31 +34,73 @@
     components: {NavigationBar},
     data:function() {
       return {
+        shows:[], //存放滚动区域要显示的数据
+        isLoading:false ,//默认没有在加载数据
         blogs: {},
         currentPage: 1,
         total: 0,
-        pageSize: 5
+        pageSize: 0,
+        pages:0 ,
+        noPage:false
       }
     },
-    mounted:function(){
-                this.page(1);
+    beforeMount() {
+    // 在页面挂载前就发起请求
+    this.page(1);
     },
     methods: {
       page(currentPage) {
-        const _this = this
+        const _this = this;
         axios.get("/" + currentPage).then(res => {
-          console.log(res)
-          _this.blogs = res.data.data.records
-          _this.currentPage = res.data.data.current
-          _this.total = res.data.data.total
-          _this.pageSize = res.data.data.size
+          console.log(currentPage);
+          _this.blogs =res.data.data.records;
+          _this.currentPage = res.data.data.current;
+          _this.total = res.data.data.total;
+          _this.pageSize = res.data.data.size;
+          _this.pages = res.data.data.pages;
+          _this.shows.push(_this.blogs);
+          console.log(_this.shows);
+           //数据请求完成
+          _this.isLoading=false;
+          
         })
-        
+      },
+    getScrollBottomHeight () {
+        // 获取整个网页的高的
+        let getPageHeight = document.querySelector('html').scrollHeight
+        // 获取滚动条卷去的高度
+        let getScrollTop = window.pageYOffset ||
+                          document.documentElement.scrollTop ||
+                          document.body.scrollTop
+        // 获取可视区域高度
+        let getWindowHeight = window.innerHeight ||
+                            document.documentElement.clientHeight
+        // 滚动条到底部的距离
+        return getPageHeight - getScrollTop - getWindowHeight
+      },
+      handleScrollsss () {
+        if (this.getScrollBottomHeight() <= 20) {
+          if(this.currentPage<=this.pages){
+            this.currentPage++;
+            this.isLoading=true;
+            this.page(this.currentPage);
+          }else{
+            this.isLoading=false;
+            this.noPage=true;
+          }
+        }
+      }
+    },
+    mounted(){
+          // 添加滚动事件
+          window.addEventListener('scroll', this.handleScrollsss)
     }
     // created() {
     //   this.page(1)
     // }
-  }}
+    }
+  
+  
 </script>
 
 <style scoped>
